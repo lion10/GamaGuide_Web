c26312b751a5bd57cf01a59b68766f898ed854bb
omar suliman abu sabha <ssomar2028@gmail.com>
2020-05-02 23:43:18 +0300

    signing user out

--- JS/auth.js
+++ JS/auth.js
@@ -14,9 +14,19 @@ signUpForm.addEventListener('submit',(e) => {
             //console.log(cred.user);
             const modal = document.querySelector('#modal-signup');
             M.Modal.getInstance(modal).close();
-            signUpForm.reset(); // close form field
-
-        
+            signUpForm.reset(); // close form field       
     });
 
 });
+
+    // logout 
+const logout = document.querySelector('#logout');
+logout.addEventListener('click',(e)=>{
+    e.preventDefault();
+
+    auth.signOut().then(()=>{
+        console.log('user sign out');
+    })
+
+})
+
