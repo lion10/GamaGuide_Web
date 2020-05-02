5c54e714394c2b2d3640c7ea3b62d25d2fc4b739
omar suliman abu sabha <ssomar2028@gmail.com>
2020-05-02 23:32:09 +0300

    signup form backend so now the use can create an account

---
+++ JS/auth.js
@@ -0,0 +1,22 @@
+const signUpForm = document.querySelector('#signup-form');
+
+signUpForm.addEventListener('submit',(e) => {
+    
+    e.preventDefault(); // prevent refresh the form aften submit info
+
+    // get user info 
+    const email = signUpForm['signup-email'].value;
+    const password = signUpForm['signup-password'].value;
+    
+
+    // signup the user 
+    auth.createUserWithEmailAndPassword(email,password).then(cred =>{
+            //console.log(cred.user);
+            const modal = document.querySelector('#modal-signup');
+            M.Modal.getInstance(modal).close();
+            signUpForm.reset(); // close form field
+
+        
+    });
+
+});
